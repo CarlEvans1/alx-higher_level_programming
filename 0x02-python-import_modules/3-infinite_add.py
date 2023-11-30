f215f8fa8fe6baf3353919c243ec9fe95f13b04f
CarlEvans1 <carltonokoth94@gmail.com>
2023-11-30 19:04:46 +0300

    print value

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys, math
     result = 0
     for i in sys.argv:
-        result += int ()
+        result = result + int(i)
         print("{}".format(result))
