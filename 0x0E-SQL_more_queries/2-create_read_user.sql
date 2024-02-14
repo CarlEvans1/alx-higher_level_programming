2a228b9d5b6cec198e6a532ccfe564f19f6d01ae
CarlEvans1 <carltonokoth94@gmail.com>
2024-02-15 01:49:54 +0300

    create database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
-CREATE USER 
+-- creates database
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+USE `hbtn_0d_2`;
+CREATE USER
 	IF NOT EXISTS 'user_0d_2'@'localhost' 
 	IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT 
-	ON hbtn_0d_2 
+GRANT SELECT
+	ON `hbtn_0d_2`.*
 	TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES
-
+FLUSH PRIVILEGES;
